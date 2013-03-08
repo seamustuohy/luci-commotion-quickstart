b2084f6b765d36479f422242b28f30ba4ca8a1ad
s2e <s2e@s2e-ThinkPad-X220.(none)>
2013-03-08 10:03:05 -0500

    did too much work over too much time and also may have ruined it al at the end

--- files/usr/lib/lua/luci/controller/QS/buttons.lua
+++ files/usr/lib/lua/luci/controller/QS/buttons.lua
@@ -1,29 +1,6 @@
 module("luci.controller.QS.buttons", package.seeall)
 
-function index(modules)
-   return(modules)
-end
-
-function tryNetwork(modules)
-   QS = luci.controller.QS.QS
-   --sets chosen  network config from on router or through commotion daemon
-   local uci = luci.model.uci.cursor()
-   nearbyMesh = uci:get('quickstart', 'options', 'meshName')
-   if nearbyMesh == uci:get('nodeConf', 'confInfo', 'name') then
-	  QS.commotionDaemon('apply', nearbyMesh)
-	  --TODO find out what data Josh can pass me to build a nodeConf
-	  --log('the daemon now passes me config data like magic and I place it in a nodeConf')
-   elseif luci.fs.isfile("/usr/share/commotion/configs/" .. nearbyMesh) then
-	  configFile = nearbyMesh
-	  local returns = luci.sys.call("cp " .. "/usr/share/commotion/configs/" .. configFile .. " /etc/config/nodeConf")
-	  if returns ~= 0 then
-		 error = "Error parsing config file. Please choose another config file or find and upload correct config" 
-		 return error 
-	  end
-	  QS.commotionDaemon('apply', 'nodeConf')
-   end
-   pages("next")
-   return(modules)
+function index()
 end
 
 function networkSecuritySettings(modules)
@@ -33,7 +10,141 @@ function networkSecuritySettings(modules)
    return(modules)
 end
 
+function gatewayShare(modules)
+   return modules
+end
 
 function back(modules)
    return({})
 end
+
+function netSec(modules)
+   local QS = luci.controller.QS.QS
+   local servald = false
+   local wpa = false
+   local upload = true
+   if luci.fs.isfile("/etc/commotion/profiles.d/quickstartMesh") then
+	  for line in io.lines("/etc/commotion/profiles.d/quickstartMesh") do
+		 b,c = string.find(line,"^wpakey=.*")
+		 d,e = string.find(line,"^servald=.*")
+		 if b then
+			wpa = string.sub(line,b+7,c)
+		 end
+		 if d then --I bet I could find an even more difficult set of variables to differentiate than b and d, but ill leave it at this :)
+			serval = string.sub(line,d+8,e)
+		 end
+	  end
+   end
+   if serval==false and wpa==false then
+	  QS.pages('next', 'naming')
+	  return modules
+   else
+	  QS.pages('next', 'keyfilesAndSecurity')
+	  return modules
+   end
+end
+
+function noApplications(modules)
+   local wpa = false
+   local upload = true
+   if luci.fs.isfile("/etc/commotion/profiles.d/quickstartMesh") then
+	  for line in io.lines("/etc/commotion/profiles.d/quickstartMesh") do
+		 b,c = string.find(line,"^wpakey=.*")
+		 d,e = string.find(line,"^servald=.*")
+		 if b then
+			wpa = string.sub(line,b+7,c)
+		 end
+		 if d then --I bet I could find an even more difficult set of variables to differentiate than b and d, but ill leave it at this :)
+			serval = string.sub(line,d+8,e)
+		 end
+	  end
+   end
+   if serval==false and wpa==false then
+	  luci.controller.QS.QS.pages('next', 'naming')
+	  return ({})
+   else
+	  luci.controller.QS.QS.pages('next', 'keyfilesAndSecurity')
+	  return ({})
+   end
+end
+
+
+function continueInsecure(modules)
+   luci.sys.call('sed -i s/^wpakey=.*//g /etc/commotion/profiles.d/quickstartMesh')
+   luci.sys.call('sed -i s/^servald=.*//g /etc/commotion/profiles.d/quickstartMesh')
+   luci.controller.QS.QS.pages('next', 'naming')
+   return ({})
+end
+
+function noConfigUploaded(modules)
+   luci.controller.QS.QS.log(modules)
+   for i,x in ipairs(modules) do
+	  if x == 'upload' then
+		 rem = i
+	  end
+   end
+   table.remove(modules, rem)
+   luci.controller.QS.QS.pages('next', 'wifiSharing')
+   return modules
+end
+
+function noSplash(modules)
+   luci.controller.QS.QS.log(modules)
+   for i,x in ipairs(modules) do
+	  if x == 'splashPage' then
+		 rem = i
+	  end
+   end
+   table.remove(modules, rem)
+   luci.controller.QS.QS.pages('next', 'meshApplications')
+   return modules
+end
+
+function gatewayShareOff(modules)
+   local uci = luci.model.uci.cursor()
+   uci:foreach("olsrd", "LoadPlugin",
+			   function(s)
+				  if s.library == "olsrd_dyn_gw.so.0.5" then
+					 dyn_gw = true
+				  end
+				  if dyn_gw == true then
+					 uci:delete("olsrd", s['.name'])
+					 uci:save('olsrd')
+					 uci:commit('olsrd')
+				  end
+			   end)
+   luci.controller.QS.QS.pages('next', 'splashPage')
+   return modules
+end
+
+function downloader(filename, name)
+   local fp = io.open(filename, "r")
+   if (fp) then
+	  luci.http.prepare_content("application/force-download")
+	  luci.http.header("Content-Disposition", "attachment; filename=" .. name)
+	  e, es = luci.ltn12.pump.all(luci.ltn12.source.file(fp), luci.http.write)
+	  fp:close()
+   end
+end
+
+function checkKeyFile(modules)
+   local val = luci.http.formvalue()
+   if val.netSec_servald == 'true' then	  
+	  --TODO Get rid of this test code for downloading
+	  --luci.sys.call("servald keyring add")
+	  --downloader("/etc/serval/serval.keyring", 'meshSettings")
+	  downloader("/etc/commotion/profiles.d/quickstartMesh", "meshSettings")
+   end
+   return {}
+end
+
+function finish()
+   local uci = luci.model.uci.cursor()
+   uci:set('quickstart', 'options', 'complete', 'true')
+   uci:save('quickstart')
+   uci:commit('quickstart')
+   luci.http.redirect("/cgi-bin/luci/admin")
+   do return end
+end
+
+
