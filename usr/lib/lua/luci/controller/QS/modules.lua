5331d5776cacdb7e5a75dd17b0a7ae2b55ea68a9
s2e <s2e@s2e-ThinkPad-X220.(none)>
2013-03-20 10:03:22 -0400

    added quotes where quotes were needed

--- usr/lib/lua/luci/controller/QS/modules.lua
+++ usr/lib/lua/luci/controller/QS/modules.lua
@@ -273,7 +273,7 @@ function nodeNamingParser()
    if val.nodeNaming_netName == '' then
 	  errors['net_name'] = "Please enter a network name"
    else
-	  nodeID = luci.sys.exec(commotion nodeid)
+	  nodeID = luci.sys.exec("commotion nodeid")
 	  hostNamen = val.nodeNaming_netName .. nodeID
 	  uci:foreach("system", "system",
 				  function(s)
